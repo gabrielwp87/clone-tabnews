466dbc2a6741f24b3dfc33602ccaecf11b438e19
Gabriel Piazenski <gabrielpiazenski@gmail.com>
2025-12-16 15:07:02 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <h1>“The me of today is not like the me of yesterday. 
+        Similarly, the me of tomorrow will be nothing like the 
+        me of today,”</h1>
+}
+
+export default Home;
